6e413df482ffdaf014282c1a7171ddb1216d3756
ahhkobe24 <ahhkobe24@gmial.com>
2017-12-25 13:16:21 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 
+const port = process.env.PORT ||3000;
+
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+   var now = new Date().toString();
+   var log = `${now}: ${req.method} ${req.url}`;
+   console.log(log);
+   next();
+});
+
 app.use(express.static(__dirname+'/public'));
 
 hbs.registerHelper('getCurrentYear',()=>{
@@ -31,7 +41,7 @@ app.get('/about',(req,res)=>{
 });
 
 
-app.listen(3000,()=>
+app.listen(port,()=>
 {
-    console.log('Server is up on port 3000');
+    console.log(`Server is up on port ${port}`);
 });
